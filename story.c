7faae83724560409c03571e2e2748d1c13a331d3
Mohamed El Hmouid <129859839+elhmouid@users.noreply.github.com>
2023-06-15 13:02:00 +0100

    Update story.c
    The provided code is a narrative-based interactive program that tells the story of an adventurer named Alex and their exploration of a mysterious forest. The program presents a series of choices to the user, allowing them to guide Alex's journey and determine the outcome of each situation.
    
    The code begins with a brief introduction to set the scene and establish the protagonist's name and their decision to explore the forest. It then proceeds to present the first question: whether Alex should take a peculiar path or continue on the main trail.
    
    Based on the user's input, the program branches into different storylines. Each storyline represents a unique sequence of events and choices that Alex encounters during their exploration. The code utilizes nested if statements to handle the user's choices and progress the story accordingly.
    
    To improve the code's design, a askQuestion function is introduced. This function takes a question, along with two options, and handles the user's input. It displays the question, waits for the user to provide a valid input (either 'A' or 'B'), and then prints the corresponding outcome based on the user's choice. This function helps eliminate code repetition and enhances readability by encapsulating the logic for asking questions and displaying outcomes.
    
    The code structure is organized in a sequential manner, with each question and subsequent outcome presented one after another. By utilizing the askQuestion function, the code becomes more modular, making it easier to add or modify questions and storylines in the future.
    
    The code also includes error handling for invalid user inputs. If the user enters an option other than 'A' or 'B', an error message is displayed, and the program terminates with an exit code of 1.
    
    Overall, the code provides an interactive storytelling experience, allowing users to guide the protagonist's choices and discover the consequences of their decisions within the context of the forest exploration narrative.

--- story.c
+++ story.c
@@ -1,98 +1,46 @@
 #include <stdio.h>
 
+void askQuestion(char *question, char *optionA, char *optionB) {
+    printf("%s (A/B)\n", question);
+    char answer;
+    scanf(" %c", &answer);
+
+    if (answer == 'A') {
+        printf("%s\n", optionA);
+    } else if (answer == 'B') {
+        printf("%s\n", optionB);
+    } else {
+        printf("Invalid input. Please choose either A or B.\n");
+        exit(1);
+    }
+}
+
 int main() {
     printf("Once upon a time, in a small village nestled in the heart of a dense forest, lived a young adventurer named Alex.\nOne sunny morning, Alex decided to explore the mysterious forest that surrounded the village.\nAs Alex ventured deeper into the woods, a peculiar path caught their attention.\n");
 
-    printf("Question 1: Should Alex take the peculiar path or continue on the main trail? (A/B)\n");
-    char answer1;
-    scanf(" %c", &answer1);
+    askQuestion("Question 1: Should Alex take the peculiar path or continue on the main trail?",
+                "Intrigued by the unknown, Alex followed the peculiar path.\nAs they walked, the foliage grew thicker, and a sense of anticipation filled the air.\nSuddenly, Alex stumbled upon an ancient treasure chest half-buried in the ground.",
+                "Preferring to stay on the safe side, Alex continued on the main trail.\nThe path led them to a peaceful meadow where colorful wildflowers bloomed and gentle butterflies fluttered around.");
 
-    if (answer1 == 'A') {
-        printf("Intrigued by the unknown, Alex followed the peculiar path.\nAs they walked, the foliage grew thicker, and a sense of anticipation filled the air.\nSuddenly, Alex stumbled upon an ancient treasure chest half-buried in the ground.\n");
+    askQuestion("Question 2: Should Alex open the treasure chest or leave it undisturbed?",
+                "With trembling hands, Alex opened the treasure chest, revealing a dazzling array of jewels and gold.\nOverwhelmed with excitement, Alex couldn't resist taking a handful of treasure.",
+                "Respecting the unknown, Alex decided not to disturb the treasure chest and continued their journey.\nThe path eventually led them to a hidden waterfall cascading down from a rocky cliff.");
 
-        printf("Question 2: Should Alex open the treasure chest or leave it undisturbed? (A/B)\n");
-        char answer2;
-        scanf(" %c", &answer2);
+    askQuestion("Question 3: Should Alex share the treasure with the village or keep it all for themselves?",
+                "With a heart full of generosity, Alex shared the treasure with the village. The villagers rejoiced, and their lives were forever changed for the better.\nAlex became a beloved hero in the community.",
+                "Greed clouded Alex's judgment as they decided to keep the treasure for themselves. Word spread about their secret, and the village turned against Alex, leaving them isolated and filled with regret.");
 
-        if (answer2 == 'A') {
-            printf("With trembling hands, Alex opened the treasure chest, revealing a dazzling array of jewels and gold.\nOverwhelmed with excitement, Alex couldn't resist taking a handful of treasure.\n");
+    askQuestion("Question 4: Should Alex take a refreshing swim in the waterfall or move on?",
+                "Alex embraced the cool, refreshing waters of the waterfall. They spent hours swimming and playing, feeling rejuvenated and at peace.\nIt became a cherished memory in their adventurous journey.",
+                "With a sense of purpose, Alex left the enchanting waterfall behind and continued their exploration.\nAs they ventured deeper into the forest, new mysteries and adventures awaited them.");
 
-            printf("Question 3: Should Alex share the treasure with the village or keep it all for themselves? (A/B)\n");
-            char answer3;
-            scanf(" %c", &answer3);
+    askQuestion("Question 5: Should Alex take a short nap or head back to the village?",
+                "In the tranquil meadow, Alex lay down amidst the wildflowers, basking in the warm sunlight.\nTime seemed to stand still as they enjoyed the serenity of nature.",
+                "Returning to the village, Alex shared tales of the beautiful meadow and the wonders of the forest.\nThe villagers were inspired by their journey and eagerly awaited their next adventure.");
 
-            if (answer3 == 'A') {
-                printf("With a heart full of generosity, Alex shared the treasure with the village. The villagers rejoiced, and their lives were forever changed for the better.\nAlex became a beloved hero in the community.\n");
-            } else if (answer3 == 'B') {
-                printf("Greed clouded Alex's judgment as they decided to keep the treasure for themselves. Word spread about their secret, and the village turned against Alex, leaving them isolated and filled with regret.\n");
-            } else {
-                printf("Invalid input. Please choose either A or B.\n");
-                return 1;
-            }
-        } else if (answer2 == 'B') {
-            printf("Respecting the unknown, Alex decided not to disturb the treasure chest and continued their journey.\nThe path eventually led them to a hidden waterfall cascading down from a rocky cliff.\n");
-
-            printf("Question 3: Should Alex take a refreshing swim in the waterfall or move on? (A/B)\n");
-            char answer3;
-            scanf(" %c", &answer3);
-
-            if (answer3 == 'A') {
-                printf("Alex embraced the cool, refreshing waters of the waterfall. They spent hours swimming and playing, feeling rejuvenated and at peace.\nIt became a cherished memory in their adventurous journey.\n");
-            } else if (answer3 == 'B') {
-                printf("With a sense of purpose, Alex left the enchanting waterfall behind and continued their exploration.\nAs they ventured deeper into the forest, new mysteries and adventures awaited them.\n");
-            } else {
-                printf("Invalid input. Please choose either A or B.\n");
-                return 1;
-            }
-        } else {
-            printf("Invalid input. Please choose either A or B.\n");
-            return 1;
-        }
-    } else if (answer1 == 'B') {
-        printf("Preferring to stay on the safe side, Alex continued on the main trail.\nThe path led them to a peaceful meadow where colorful wildflowers bloomed and gentle butterflies fluttered around.\n");
-
-        printf("Question 2: Should Alex enjoy the serenity of the meadow or keep exploring the forest? (A/B)\n");
-        char answer2;
-        scanf(" %c", &answer2);
-
-        if (answer2 == 'A') {
-            printf("In the tranquil meadow, Alex lay down amidst the wildflowers, basking in the warm sunlight.\nTime seemed to stand still as they enjoyed the serenity of nature.\n");
-
-            printf("Question 3: Should Alex take a short nap or head back to the village? (A/B)\n");
-            char answer3;
-            scanf(" %c", &answer3);
-
-            if (answer3 == 'A') {
-                printf("Alex took a short nap amidst the peaceful meadow. When they woke up, the sun was beginning to set, casting a golden hue over the landscape.\nIt was time to head back to the village, feeling refreshed and content.\n");
-            } else if (answer3 == 'B') {
-                printf("Returning to the village, Alex shared tales of the beautiful meadow and the wonders of the forest.\nThe villagers were inspired by their journey and eagerly awaited their next adventure.\n");
-            } else {
-                printf("Invalid input. Please choose either A or B.\n");
-                return 1;
-            }
-        } else if (answer2 == 'B') {
-            printf("Driven by curiosity, Alex ventured further into the forest.\nAmong the trees, they discovered an ancient stone tablet with mysterious engravings.\n");
-
-            printf("Question 3: Should Alex try to decipher the engravings or leave them be? (A/B)\n");
-            char answer3;
-            scanf(" %c", &answer3);
-
-            if (answer3 == 'A') {
-                printf("Driven by curiosity, Alex deciphered the engravings on the ancient stone tablet.\nThey unveiled the hidden history of the forest, uncovering forgotten secrets and becoming the guardian of ancient knowledge.\n");
-            } else if (answer3 == 'B') {
-                printf("Respecting the mysteries of the forest, Alex left the engravings undisturbed.\nThey continued their exploration, forever carrying the sense of wonder and respect for the unknown.\n");
-            } else {
-                printf("Invalid input. Please choose either A or B.\n");
-                return 1;
-            }
-        } else {
-            printf("Invalid input. Please choose either A or B.\n");
-            return 1;
-        }
-    } else {
-        printf("Invalid input. Please choose either A or B.\n");
-        return 1;
-    }
+    askQuestion("Question 6: Should Alex try to decipher the engravings or leave them be?",
+                "Driven by curiosity, Alex deciphered the engravings on the ancient stone tablet.\nThey unveiled the hidden history of the forest, uncovering forgotten secrets and becoming the guardian of ancient knowledge.",
+                "Respecting the mysteries of the forest, Alex left the engravings undisturbed.\nThey continued their exploration, forever carrying the sense of wonder and respect for the unknown.");
 
     return 0;
 }
